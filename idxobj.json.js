b2aad3cdbc4feb26a5db5a7d7990258aee10187c
wdingsoft <wdingbox@gmail.com>
2022-01-28 01:29:03 -0500

    BrushPen 300BC

---
+++ idxobj.json.js
@@ -0,0 +1,108 @@
+var idxobj = {
+    "./": [
+        {
+            "idxobj.json.js": 2469
+        },
+        {
+            "idxor.html": 1495
+        }
+    ],
+    "src": [
+        {
+            "jx_letter2gov_2021_10_24.m4a": 40960862
+        }
+    ],
+    "src/PennMuseum_obi": [
+        {
+            "Images-49-14-7A-OracleBone_Collections-PennMuseum.html": 31100
+        },
+        {
+            "Images-49-14-7B-OracleBone_Collections-PennMuseum.html": 31105
+        },
+        {
+            "Images-65-2-8-OracleBone_Collections-PennMuseum.html": 31052
+        },
+        {
+            "Images-65-2-9-OracleBone_Collections-PennMuseum.html": 31239
+        },
+        {
+            "Images55-34-7-OracleBone_Collections-PennMuseum.html": 31243
+        }
+    ],
+    "src/PennMuseum_obi/Images-49-14-7A-OracleBone_Collections-PennMuseum_files": [
+        {
+            "362256.jpg": 166292
+        },
+        {
+            "362259.jpg": 181577
+        }
+    ],
+    "src/PennMuseum_obi/Images-49-14-7B-OracleBone_Collections-PennMuseum_files": [
+        {
+            "362257.jpg": 109284
+        },
+        {
+            "362258.jpg": 146876
+        }
+    ],
+    "src/PennMuseum_obi/Images-65-2-8-OracleBone_Collections-PennMuseum_files": [
+        {
+            "362264.jpg": 108632
+        },
+        {
+            "362265.jpg": 109483
+        }
+    ],
+    "src/PennMuseum_obi/Images-65-2-9-OracleBone_Collections-PennMuseum_files": [
+        {
+            "362267.jpg": 153566
+        },
+        {
+            "362268.jpg": 142862
+        },
+        {
+            "362269.jpg": 129026
+        }
+    ],
+    "src/PennMuseum_obi/Images55-34-7-OracleBone_Collections-PennMuseum_files": [
+        {
+            "362261.jpg": 175928
+        },
+        {
+            "362262.jpg": 181589
+        },
+        {
+            "362263.jpg": 170518
+        }
+    ],
+    "src/docs": [
+        {
+            "Zhangetal_2016_OsteoarchaeologicalStudiesofHumanSystemicStressofEarlyUrbanizationinLateShangatAnyangChina.pdf.pdf": 1267898
+        }
+    ],
+    "src/dragon": [
+        {
+            "10.jpg": 26540
+        },
+        {
+            "EAH36_05.pdf": 994683
+        },
+        {
+            "Qianlong.png": 2910497
+        },
+        {
+            "notes.htm": 1965
+        }
+    ],
+    "src/pics": [
+        {
+            "anyan_himan_bones.jpg": 1307119
+        },
+        {
+            "anyan_himan_bones.txt": 147
+        },
+        {
+            "dsc01249.jpg": 1528579
+        }
+    ]
+}
